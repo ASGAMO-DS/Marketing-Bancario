d17ff269b763e82549a3e9b396c88f84e633658a
ASGAMO-DS <alejandro229208@gmail.com>
2025-02-01 21:57:04 +0000

    Visualización de duplicados (no funciona la funcion desde Utils)

--- src/utils.py
+++ src/utils.py
@@ -11,3 +11,9 @@ def db_connect():
     engine = create_engine(os.getenv('DATABASE_URL'))
     engine.connect()
     return engine
+
+def valores_duplicados(df):
+    for columna in df.columns:
+        suma = df[columna].duplicated().sum()
+        print(f'en la columna {columna} hay {suma} valores duplicados')
+    
