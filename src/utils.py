19aaa078b498b13ed802dda4f337275460ab7279
ASGAMO-DS <alejandro229208@gmail.com>
2025-02-06 23:26:36 +0000

    Modelo completo con efectividad del 89.55%

--- src/utils.py
+++ src/utils.py
@@ -16,4 +16,17 @@ def valores_duplicados(df):
     for columna in df.columns:
         suma = df[columna].duplicated().sum()
         print(f'en la columna {columna} hay {suma} valores duplicados')
-    
+
+def optimización_hiperparametros_Reg_logistica():
+  
+  from sklearn.model_selection import GridSearchCV
+hyperparams = {
+    "C": [0.001, 0.01, 0.1, 1, 10, 100, 1000],
+    "penalty": ["l1", "l2", "elasticnet", None],
+    "solver": ["newton-cg", "lbfgs", "liblinear", "sag", "saga"]
+}
+
+grid = GridSearchCV(model,hyperparams,scoring='accuracy',cv=5)
+}
+
+grid = GridSearchCV(model,hyperparams,scoring='accuracy',cv=5)
